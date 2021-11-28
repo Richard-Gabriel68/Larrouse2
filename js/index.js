9157afd8c2a9758ac2703a38cef4fbd1f7c6ae03
Richard <rickanimals02@gmail.com>
2021-11-28 16:59:41 -0200

    Parte pizzas e modal

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const s = (e) => document.querySelector(e)
+
+pizzaJson.map((item, index) => {
+    let pizzaItem = s(".models .element").cloneNode(true);
+    let modal = s(".fundoModal");
+    let closeButton = s(".modal .closeModal");
+
+    pizzaItem.querySelector(".name").innerHTML = item.name;
+    pizzaItem.querySelector(".price").innerHTML = item.price.toFixed(2);
+    pizzaItem.querySelector("img").src = item.img;
+    pizzaItem.addEventListener("click", (e) => {
+        modal.style.opacity = 0;
+        modal.style.display = "flex";
+        setTimeout ( ()=> {
+            modal.style.opacity = 1;
+        }, 500)
+
+        modal.querySelector(".campoPizza img").src = item.img;
+        modal.querySelector(".nomePizza").innerHTML = item.name;
+        modal.querySelector(".descPizza").innerHTML = item.description;
+        modal.querySelector(".valorPizza").innerHTML = item.price;
+
+        closeButton.addEventListener("click", (e)=> {
+            modal.style.display = "none";
+        })
+    })
+
+    document.querySelector("#pizzas .content").append(pizzaItem);
+})
